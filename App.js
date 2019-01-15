6a97a1a59d2efa7689b5d831fd0bec52d9fd559b
fernandes-wiraharjo <fernandeswiraharjo@gmail.com>
2019-01-15 21:37:12 +0700

    first commit app

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { View } from 'react-native';
+import Profile from "./src/Profile"
+
+class App extends Component {
+  render() {
+    return (
+      <Profile />
+    );
+  }
+}
+
+export default App
